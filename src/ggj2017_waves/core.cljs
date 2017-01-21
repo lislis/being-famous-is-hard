c0d4a6c09ea1ba985860e86c77386b343868dd0f
lislis <mail@lislis.de>
2017-01-21 16:49:29 +0100

    it waves!!

--- src/ggj2017_waves/core.cljs
+++ src/ggj2017_waves/core.cljs
@@ -5,42 +5,53 @@
 (defonce game (p/create-game 600 400))
 (defonce state (atom {:player-x 100
                       :player-y 40
-                      :player-image nil}))
+                      :player-image (p/load-image game "player-1.png")
+                      :player-wave (p/load-image game "player-2.png")
+                      :player-is-waving? false
+                      :bg (p/load-image game "city.png")
+                      :fruit-store (p/load-image game "fruit.png")
+                      :croissant-store (p/load-image game "croissant.png")
+                      :coffee-store (p/load-image game "coffee.png")
+                      }))
 
 (def speed 10)
 
-(defn move [direction]
-  (case direction
-    :left (swap! state assoc :player-x (- (:player-x @state) speed))
-    :right (swap! state assoc :player-x (+ (:player-x @state) speed))
-    :up (swap! state assoc :player-y (- (:player-y @state) speed))
-    :down (swap! state assoc :player-y (+ (:player-y @state) speed))
-    false))
+(defn move
+  "The player can only move if they're not waving.
+  Walking _and_ waving is just not cool"
+  [direction]
+  (if (false? (:player-is-waving? @state))
+    (case direction
+      :left (swap! state assoc :player-x (- (:player-x @state) speed))
+      :right (swap! state assoc :player-x (+ (:player-x @state) speed))
+      :up (swap! state assoc :player-y (- (:player-y @state) speed))
+      :down (swap! state assoc :player-y (+ (:player-y @state) speed))
+      false)))
+
+(defn wave []
+  (swap! state assoc :player-is-waving? (not (true? (:player-is-waving? @state)))))
 
 (def main-screen
   (reify p/Screen
-    (on-show [this]
-      (swap! state assoc
-             :player-image (p/load-image game "player-1.png")
-             :bg (p/load-image game "city.png")))
+    (on-show [this])
 
     (on-hide [this])
 
     (on-render [this]
-      (p/render game
-                [[:div {:x 0 :y 0}
-                  [:image {:value (:bg @state)}]]
-                 [:image {:value (:player-image @state) :x (:player-x @state) :y (:player-y @state)}]
-                 ]))))
+      (let [player (if (:player-is-waving? @state) (:player-wave @state) (:player-image @state))]
+        (p/render game
+                  [[:div {:x 0 :y 0}
+                    [:image {:value (:bg @state)}]
+                    [:image {:value (:fruit-store @state) :x 50 :y 50}]
+                    [:image {:value (:croissant-store @state) :x 210 :y 280}]
+                    [:image {:value (:coffee-store @state) :x 430 :y 130}]]
+                   [:image {:value player :x (:player-x @state) :y (:player-y @state)}]
+                   ])))))
 
 (doto game
   (p/stop)
   (p/start)
   (p/set-screen main-screen))
-
-;;(events/listen js/window "mousemove"
-;;               (fn [^js/KeyboardEvent event]
-;;                 (js/console.log event)))
 
 (events/listen js/window "keydown"
                (fn [^js/KeyboardEvent event]
@@ -50,5 +61,13 @@
                      65 (move :left)   ; a
                      83 (move :down)   ; s
                      68 (move :right)  ; d
+                     false))))
+
+;; using keyup as it is not firing constantly
+(events/listen js/window "keyup"
+               (fn [^js/KeyboardEvent event]
+                 (let [key (.-keyCode event)]
+                   (case key
+                     32 (wave)         ; space
                      false)
                    (js/console.log key))))
